6b63073c7a6b93368e4ae47664dd2a30ef10d43d
chenkaiyu1997 <justryit8@gmail.com>
2018-08-28 13:46:21 +0800

    delete build

--- devel/share/candy/cmake/candy-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(candy_MESSAGE_FILES "")
-set(candy_SERVICE_FILES "/home/linpan/github/ros_candy/src/candy/srv/Step.srv;/home/linpan/github/ros_candy/src/candy/srv/Value.srv;/home/linpan/github/ros_candy/src/candy/srv/UpdateWeights.srv")
--- devel/share/common-lisp/ros/candy/srv/_package_UpdateWeights.lisp
+++
@@ -1,6 +0,0 @@
-(cl:in-package candy-srv)
-(cl:export '(A-VAL
-          A
-          B-VAL
-          B
-))
