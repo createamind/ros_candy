94cc213631992a0ad09160862634c2131af32a1c
chenkaiyu1997 <justryit8@gmail.com>
2018-08-29 11:19:32 +0800

    lower prioritized replay

--- src/candy/src/trainer_candy.py
+++ src/candy/src/trainer_candy.py
@@ -50,9 +50,9 @@ if __name__ == '__main__':
 
 		global batch
 		global global_step	
-		if len(batch) > 1000:
-			batch = batch[:1000]
-		if global_step % (TRAIN_EPOCH * 30) == 0:
+		if len(batch) > 300:
+			batch = batch[:300]
+		if global_step % (TRAIN_EPOCH * 10) == 0:
 			batch = []
 		l = len(obs)
 		for i in range(12, l - 12):
