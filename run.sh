cf60a96b230652a2e8828bba6128d2c2b37f2271
chenkaiyu1997 <justryit8@gmail.com>
2018-09-10 14:04:37 +0800

    change back to stable

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export NAME=finalbigdata
+export NAME=imitationppo5ppobigdata
 export PORT=10001
 export ROS_MASTER_URI=http://jz:$PORT/
 export CUDA_VISIBLE_DEVICES=2
@@ -8,5 +8,5 @@ roscore -p $PORT&
 sleep 2
 rosrun candy wrapper_candy.py -l -t -n -d&
 rosrun candy trainer_candy.py -u -n $NAME&
-rosbag play /data/0828/*.bag /data/0829/*.bag /data/0830/*.bag /data/0905/*.bag -l -r 10 --skip-empty=10
-# rosbag play /data/0828/left2.bag -l -r 10 --skip-empty=10
+# rosbag play /data/0828/*.bag /data/0829/*.bag /data/0830/*.bag /data/0905/*.bag -l -r 10 --skip-empty=10
+rosbag play /data/0828/left2.bag -l -r 10 --skip-empty=10
