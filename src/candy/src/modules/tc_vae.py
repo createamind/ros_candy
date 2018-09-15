8425e193949199699dbe3b1484b67c2b74860aec
xlnwel <122134545@qq.com>
2018-09-15 20:26:02 +0800

    fix bugs

--- src/candy/src/modules/tc_vae.py
+++ src/candy/src/modules/tc_vae.py
@@ -3,10 +3,14 @@ from modules.utils.distribution import log_normal
 import modules.utils.utils as utils
 from math import log
 
+""""
+I comment out things corresponding to computing 'constant' in self._loss()
+since 'constant' seems to contribute nothing to back-propagation 
+"""
 
 class TCVAE(BetaVAE):
     def __init__(self, name, args, reuse=False):
-        self.dataset_size = args['dataset_size']
+        # self.dataset_size = args['dataset_size']
         self.alpha = args[self._name]['alpha']
         self.beta = args[self._name]['beta']
         self.gamma = args[self._name]['gamma']
@@ -16,30 +20,35 @@ class TCVAE(BetaVAE):
     def _loss(self, mu, logsigma, predictions, labels):
         with tf.variable_scope('loss', reuse=self.reuse):
             with tf.variable_scope('kl_loss', reuse=self.reuse):
-                logpz = log_normal(self.sample_z, mu, logsigma)
-                logqz_condx = log_normal(self.sample_z, mu, logsigma)
+                logpz = log_normal(self.sample_z, 0, 0)                     # log(p(z))
+                logqz_condx = log_normal(self.sample_z, mu, logsigma)       # log(q(z|x))
 
+                # log(q|z)
                 logqz_condx_expanded = log_normal(tf.expand_dims(self.sample_z, axis=1), 
                                                   tf.expand_dims(mu, axis=0), 
                                                   tf.expand_dims(logsigma, axis=0))
 
-                constant = log(self.batch_size * self.dataset_size)
-                logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False) \
-                                                                 - constant, axis=1)
-                logqz = utils.logsumexp(tf.reduce_sum(logqz_condx_expanded, axis=2), axis=1, keepdims=False) - constant
+                # constant = log(self.batch_size * self.dataset_size)
+                # logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False) - constant, axis=1)
+                # sum(log(sum(q(zi|x))))
+                logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False), axis=1)
+                # log(sum(q(z|x)))
+                logqz = utils.logsumexp(tf.reduce_sum(logqz_condx_expanded, axis=2), axis=1, keepdims=False)# - constant
 
-                MI_loss = logqz_condx - logqz
-                TC_loss = logqz - logqz_marginal_product
-                Dimension_wise_KL = logqz_marginal_product - logpz
+                MI_loss = tf.reduce_mean(logqz_condx - logqz)
+                TC_loss = tf.reduce_mean(logqz - logqz_marginal_product)
+                Dimension_wise_KL = tf.reduce_mean(logqz_marginal_product - logpz)
 
                 KL_loss = self.alpha * MI_loss + self.beta * TC_loss + self.gamma * Dimension_wise_KL
 
             with tf.variable_scope('reconstruction_error', reuse=self.reuse):
                 reconstruction_loss = utils.mean_square_error(labels, predictions)
             
-            l2_loss = t f.losses.get_regularization_loss(self._name, name='l2_loss')
+            with tf.variable_scope('l2_regularization', reuse=self.reuse):
+                l2_loss = t f.losses.get_regularization_loss(self._name, name='l2_loss')
             
-            loss = reconstruction_loss + KL_loss + l2_loss
+            with tf.variable_scope('total_loss', reuse=self.reuse):
+                loss = reconstruction_loss + KL_loss + l2_loss
 
             tf.summary.scalar('Reconstruction_error', reconstruction_loss)
             tf.summary.scalar('MI_loss', MI_loss)
@@ -49,5 +58,5 @@ class TCVAE(BetaVAE):
             tf.summary.scalar('L2_loss', l2_loss)
             tf.summary.scalar('Total_loss', loss)
 
-            return loss
+        return loss
 
