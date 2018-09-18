eb34e9b235ec0076fd792a65221f7444174e9da4
xlnwel <122134545@qq.com>
2018-09-18 18:29:56 +0800

    Add the constant

--- src/candy/src/modules/tc_vae.py
+++ src/candy/src/modules/tc_vae.py
@@ -4,15 +4,11 @@ import modules.utils.utils as utils
 import numpy as np
 import tensorflow as tf
 
-""""
-I comment out things corresponding to computing 'constant' in self._loss()
-since 'constant' seems to contribute nothing to back-propagation 
-"""
-
 class TCVAE(BetaVAE):
     """ Interface """
     def __init__(self, name, args, reuse=False):
-        # self.dataset_size = args['dataset_size']
+        self.dataset_size = args['dataset_size']
+        self.batch_size = args['batch_size']
         self.alpha = args[name]['alpha']
         self.beta = args[name]['beta']
         self.gamma = args[name]['gamma']
@@ -23,28 +19,32 @@ class TCVAE(BetaVAE):
     def _loss(self, mu, logsigma, predictions, labels):
         with tf.name_scope('loss'):
             with tf.name_scope('kl_loss'):
-                logpz = tf.reduce_sum(log_normal(self.sample_z, 0., 0.), axis=1)                       # log(p(z))
-                logqz_condx = tf.reduce_sum(log_normal(self.sample_z, mu, logsigma), axis=1)           # log(q(z|x))
+                kl_penalizing_size = self.z_size // 2
+                sample_z_penalized = self.sample_z[:, : kl_penalizing_size]
+                mu_penalized = mu[:, : kl_penalizing_size]
+                logsigma_penalized = logsigma[:, : kl_penalizing_size]
+                logpz = tf.reduce_sum(log_normal(sample_z_penalized, 0., 0.), axis=1)                                           # log(p(z))
+                logqz_condx = tf.reduce_sum(log_normal(sample_z_penalized, mu_penalized, logsigma_penalized), axis=1)           # log(q(z|x))
 
                 # log(q|z)
-                logqz_condx_expanded = log_normal(tf.expand_dims(self.sample_z, axis=1), 
-                                                  tf.expand_dims(mu, axis=0), 
-                                                  tf.expand_dims(logsigma, axis=0))
+                logqz_condx_expanded = log_normal(tf.expand_dims(sample_z_penalized, axis=1), 
+                                                  tf.expand_dims(mu_penalized, axis=0), 
+                                                  tf.expand_dims(logsigma_penalized, axis=0))
 
-                # constant = np.log(self.batch_size * self.dataset_size)
-                # logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False) - constant, axis=1)
+                constant = np.log(self.batch_size * self.dataset_size)
+                
+                # sum(log(sum(q(zi|x))) - constant)
+                logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False) - constant, axis=1)
 
-                # sum(log(sum(q(zi|x))))
-                logqz_marginal_product = tf.reduce_sum(utils.logsumexp(logqz_condx_expanded, axis=1, keepdims=False), axis=1)
-                # log(sum(q(z|x)))
-                logqz = utils.logsumexp(tf.reduce_sum(logqz_condx_expanded, axis=2), axis=1, keepdims=False)# - constant
+                # log(sum(q(z|x))) - constant
+                logqz = utils.logsumexp(tf.reduce_sum(logqz_condx_expanded, axis=2), axis=1, keepdims=False) - constant
                 
                 # divided by image_size**2 because we use MSE for reconstruction loss
-                MI_loss = tf.reduce_mean(logqz_condx - logqz) / (self._args['image_size']**2)
-                TC_loss = tf.reduce_mean(logqz - logqz_marginal_product) / (self._args['image_size']**2)
-                dimension_wise_KL = tf.reduce_mean(logqz_marginal_product - logpz) / (self._args['image_size']**2)
+                MI_loss = self.alpha * tf.reduce_mean(logqz_condx - logqz) / (self._args['image_size']**2)
+                TC_loss = self.beta * tf.reduce_mean(logqz - logqz_marginal_product) / (self._args['image_size']**2)
+                dimension_wise_KL = self.gamma * tf.reduce_mean(logqz_marginal_product - logpz) / (self._args['image_size']**2)
 
-                KL_loss = self.alpha * MI_loss + self.beta * TC_loss + self.gamma * dimension_wise_KL
+                KL_loss = MI_loss + TC_loss + dimension_wise_KL
 
             with tf.name_scope('reconstruction_error'):
                 reconstruction_loss = tf.losses.mean_squared_error(labels, predictions)
