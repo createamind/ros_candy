4d082f9a6c2c34b33d2ebb6ec8b99fdcab622185
xlnwel <122134545@qq.com>
2018-09-18 18:28:49 +0800

    Miscellanea

--- src/candy/src/modules/utils/distribution.py
+++ src/candy/src/modules/utils/distribution.py
@@ -2,17 +2,20 @@ import numpy as np
 import tensorflow as tf
 
 def log_normal(sample, mu, logsigma):
-    """ Compute a Gaussian distribution density with mean mu and standard deviation exp(logsigma)
+    """Compute a Gaussian distribution density with mean mu and standard deviation exp(logsigma)
     
     Arguments:
-        sample {[type]} -- [description]
-        
+        sample: x
+        mu: mean
+        logsigma: log of the standard deviation
+    
     Returns:
-        [type] -- [description]
+        log N(x; mu, sigma**2)
     """
+
     log2pi = tf.constant([np.log(2 * np.pi)])           # log(2pi)
     inverse_sigma = tf.exp(-logsigma)                   # 1/sigma
     tmp = (sample - mu) * inverse_sigma                 # (x - mu)/sigma
-
+    
     return -0.5 * (tmp**2 + 2 * logsigma + log2pi)      # log(N(mu, sigma))
 
