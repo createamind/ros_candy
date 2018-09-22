321f8eaf79911da2d08a04999ee6e4d63c902e75
xlnwel <122134545@qq.com>
2018-09-22 14:35:33 +0800

    Add log option

--- src/candy/src/modules/dim.py
+++ src/candy/src/modules/dim.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 import tensorflow as tf
 from modules.beta_vae import BetaVAE
@@ -7,9 +6,10 @@ import modules.utils.tf_utils as tf_utils
 
 class DIM(BetaVAE):
     """ Interface """
-    def __init__(self, name, args, reuse=False):
-        super(DIM, self).__init__(name, args, reuse)
+    def __init__(self, name, args, reuse=False, build_graph=True, log_tensorboard=False):
+        super(DIM, self).__init__(name, args, reuse, build_graph=build_graph, log_tensorboard=log_tensorboard)
 
+    """ Implementation """
     def _loss(self, mu, logsigma, predictions, labels):
         with tf.variable_scope('loss', reuse=self.reuse):
             with tf.variable_scope('local_MI', reuse=self.reuse):
@@ -26,26 +26,29 @@ class DIM(BetaVAE):
             with tf.name_scope('total_loss'):
                 loss = reconstruction_loss - 10 * local_MI + l2_loss
 
-            tf.summary.scalar('T_joint_', tf.reduce_mean(T_joint))
-            tf.summary.scalar('T_prod_', tf.reduce_mean(T_prod))
-            tf.summary.scalar('E_joint_', E_joint)
-            tf.summary.scalar('E_prod_', E_prod)
-            tf.summary.scalar('Reconstruction_error_', reconstruction_loss)
-            tf.summary.scalar('Local_MI_', local_MI)
-            tf.summary.scalar('L2_loss_', l2_loss)
-            tf.summary.scalar('Total_loss_', loss)
+            if self.log_tensorboard:
+                tf.summary.scalar('E_joint_', E_joint)
+                tf.summary.scalar('E_prod_', E_prod)
+                tf.summary.scalar('Reconstruction_error_', reconstruction_loss)
+                tf.summary.scalar('Local_MI_', local_MI)
+                tf.summary.scalar('L2_loss_', l2_loss)
+                tf.summary.scalar('Total_loss_', loss)
         
         return loss
 
-    """ Implementation """
     def _score(self, z):
         with tf.name_scope('discriminator'):
             T_joint = self._get_score(z)
             T_prod = self._get_score(z, shuffle=True)
+            if self.log_tensorboard:
+                tf.summary.histogram('T_joint_', T_joint)
+                tf.summary.histogram('T_prod_', T_prod)
+                tf.summary.scalar('T_joint_scalar_', tf.reduce_mean(T_joint))
+                tf.summary.scalar('T_prod_scalar_', tf.reduce_mean(T_prod))
 
-            log2 = math.log(2.)
-            E_joint = log2 - tf.reduce_mean(tf.math.softplus(-T_joint))
-            E_prod = tf.reduce_mean(tf.math.softplus(-T_prod) + T_prod) - log2
+            log2 = np.log(2.)
+            E_joint = tf.reduce_mean(log2 - tf.math.softplus(-T_joint))
+            E_prod = tf.reduce_mean(tf.math.softplus(-T_prod) + T_prod - log2)
 
         return T_joint, T_prod, E_joint, E_prod
 
@@ -54,8 +57,7 @@ class DIM(BetaVAE):
             feature_map = self.dim_feature_map
             height, width, channels = feature_map.shape.as_list()[1:]
             z_channels = z.shape.as_list()[-1]
-            
-            # here's where the error is introduced
+
             if shuffle:
                 feature_map = tf.reshape(feature_map, (-1, channels))
                 feature_map = tf.random_shuffle(feature_map)
@@ -65,20 +67,24 @@ class DIM(BetaVAE):
             # expand z
             z_padding = tf.tile(z, [1, height * width])
             z_padding = tf.reshape(z_padding, [-1, height, width, z_channels])
-
+            
             feature_map = tf.concat([feature_map, z_padding], axis=-1)
+            
             scores = self._local_discriminator(feature_map, shuffle)
             scores = tf.reshape(scores, [-1, height * width])
 
         return scores
 
     def _local_discriminator(self, feature_map, reuse):
-        with tf.variable_scope('local_discriminator', reuse=reuse):
+        with tf.variable_scope('discriminator_net', reuse=reuse) as net:
             x = self._conv(feature_map, 256, 1, kernel_initializer=tf_utils.kaiming_initializer())
             x = tf.nn.relu(x)
-            x = self._conv(feature_map, 128, 1, kernel_initializer=tf_utils.kaiming_initializer())
+            x = self._conv(feature_map, 256, 1, kernel_initializer=tf_utils.kaiming_initializer())
             x = tf.nn.relu(x)
-            x = self._conv(feature_map, 1, 1)
+            x = self._conv(feature_map, 1, 1, kernel_initializer=tf_utils.kaiming_initializer())
+            x = tf.nn.relu(x)
+            
+            net.reuse_variables()
 
         return x
 
