adf00f55fb8e4eb81d4b20ba48e3552195010822
xlnwel <122134545@qq.com>
2018-09-22 14:35:46 +0800

    Add done

--- src/candy/src/modules/replaybuffer.py
+++ src/candy/src/modules/replaybuffer.py
@@ -6,22 +6,22 @@ class ReplayBuffer():
     def __init__(self, sample_size, max_len=int(1e5)):
         self.buffer = deque(maxlen=max_len)
         self.sample_size = sample_size
-        self.experience = namedtuple('experience', ('state', 'action', 'reward', 'next_state'))
+        self.experience = namedtuple('experience', ('state', 'action', 'reward', 'next_state', 'done'))
 
-    def add(self, state, action, reward, next_state):
-        exp = self.experience(state, action, reward, next_state)
+    def add(self, state, action, reward, next_state, done):
+        exp = self.experience(state, action, reward, next_state, done)
 
         self.buffer.append(exp)
 
     def sample(self):
-        # _, exps = zip(*random.sample(self.buffer, self.sample_size))
         exps = random.sample(self.buffer, self.sample_size)
         states = np.vstack([e.state for e in exps if e is not None])
         actions = np.vstack([e.action for e in exps if e is not None])
         rewards = np.vstack([e.reward for e in exps if e is not None])
         next_states = np.vstack([e.next_state for e in exps if e is not None])
+        dones = np.vstack([e.done for e in exps if e is not None])
 
-        return states, actions, rewards, next_states
+        return states, actions, rewards, next_states, dones
 
     def __len__(self):
         return len(self.buffer)
